81df47be13528e6d405399d3858e5604e34ad174
Godwill254 <simiyugodwill540@gmail.com>
2024-04-09 18:47:47 +0300

    ui done

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { FormatCurrency } from "../utilities/formatCurrecy";
 
 type StoreItemProps = {
@@ -9,8 +9,9 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imageURL }: StoreItemProps) {
+  const quantity = 1;
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={imageURL}
@@ -19,9 +20,31 @@ export function StoreItem({ id, name, price, imageURL }: StoreItemProps) {
       />
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-            <span className="fs-2" >{name}</span>
-            <span className="ms-2 text-muted">{ FormatCurrency (price)}</span>
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{FormatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity <= 0 ? (
+            <Button className="w-100">+ Add to Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column "
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm" >Remove</Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
